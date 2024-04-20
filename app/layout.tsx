364f4a4d32bf2ddcac372ca2042a4725f95eb213
shakibuljoy <shakibulalamj@gmail.com>
2024-04-20 23:47:19 +0600

    Navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+       <Header />
+      
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          
+        {children}
+        
+       </div>
+       </div>
+
+      </body>
     </html>
   );
 }
